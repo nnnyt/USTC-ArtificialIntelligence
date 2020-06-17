0b81524c57e5adbee93ef76b0b5abc60b47cb9a9
nnnyt <793313994@qq.com>
2020-06-17 20:27:27 +0800

    add LR

--- PB17111577_宁雨亭_exp2/supervise/src/main.py
+++ PB17111577_宁雨亭_exp2/supervise/src/main.py
@@ -3,14 +3,14 @@ import csv
 from sklearn.preprocessing import LabelEncoder
 import random
 from KNN import KNN
-# from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.neighbors import KNeighborsClassifier
+from LogisticRegression import LogisticRegression as LR
 
 MAT_DATASET = '../data/student/student-mat.csv'
 POR_DATASET = '../data/student/student-por.csv'
 
 
-def load_data(filename=MAT_DATASET, use_G = True):
+def load_data(filename=MAT_DATASET, algorithm='KNN', use_G = True):
     labels = []
     attributes = [[] for i in range(32)]
     with open(filename, 'r') as f:
@@ -21,7 +21,10 @@ def load_data(filename=MAT_DATASET, use_G = True):
             for i in range(32):
                 attributes[i].append(line[i])
     # print(np.array(attributes)[:,0])
-    labels = list(map(lambda x: 1 if int(x) >= 10 else 0, labels))
+    if algorithm == 'SVM':
+        labels = list(map(lambda x: 1 if int(x) >= 10 else -1, labels))
+    else:
+        labels = list(map(lambda x: 1 if int(x) >= 10 else 0, labels))
     for i in [0, 1, 3, 4, 5, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22]:
         attributes[i] = LabelEncoder().fit_transform(attributes[i])
     for i in range(len(attributes)):
@@ -54,13 +57,13 @@ def train_test_split(x, y, test_size=0.3, random_seed=212):
 def evaluate(y_true, y_pred):
     TP = FP = FN = TN = 0
     for i in range(len(y_true)):
-        if y_true[i]:
-            if y_pred[i]:
+        if y_true[i] == 1:
+            if y_pred[i] == 1:
                 TP += 1
             else:
                 FN += 1
         else:
-            if y_pred[i]:
+            if y_pred[i] == 1:
                 FP += 1
             else:
                 TN += 1
@@ -72,8 +75,9 @@ def evaluate(y_true, y_pred):
 if __name__ == '__main__':
     x, y = load_data()
     x_train, y_train, x_test, y_test = train_test_split(x, y, random_seed=200)
-    model = KNN()
+    # model = KNN()
     # model = KNeighborsClassifier(n_neighbors=10)
+    model = LR()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
     print(evaluate(y_test, y_pred))
